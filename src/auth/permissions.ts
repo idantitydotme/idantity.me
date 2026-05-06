327227d8890956e6f9fd18c0de5c7713ce0922ed
idantitydotme <88442616+idantitydotme@users.noreply.github.com>
2026-05-05 23:18:57 -0300

    feat: updates auth permissions

--- src/auth/permissions.ts
+++ src/auth/permissions.ts
@@ -8,29 +8,61 @@ import {
 
 export const statement = {
   ...defaultStatements,
-  blogPost: ["create", "update", "review", "delete"],
-  legal: ["create", "update", "review", "delete"]
+  organization: ["create", "edit", "delete"],
+  team: ["create", "edit", "delete"],
+  member: ["create", "edit", "delete"],
+  invitation: ["create", "delete"],
+  project: ["create", "edit", "delete"],
+  asset: ["view", "create", "edit", "delete"],
+  blogPost: ["create", "edit", "review", "delete"],
+  legal: ["create", "edit", "review", "delete"]
 } as const
 
 export const ac = createAccessControl(statement)
 
 export const owner = ac.newRole({
-  blogPost: ["create", "update", "review", "delete"],
-  legal: ["create", "update", "review", "delete"],
-  ...ownerAc.statements
+  ...ownerAc.statements,
+  organization: ["create", "edit", "delete"],
+  team: ["create", "edit", "delete"],
+  member: ["create", "edit", "delete"],
+  invitation: ["create", "delete"],
+  project: ["create", "edit", "delete"],
+  asset: ["view", "create", "edit", "delete"],
+  blogPost: ["create", "edit", "review", "delete"],
+  legal: ["create", "edit", "review", "delete"]
 })
 
 export const admin = ac.newRole({
-  blogPost: ["create", "update", "review", "delete"],
-  legal: ["create", "update", "review", "delete"],
-  ...adminAc.statements
+  ...adminAc.statements,
+  organization: [],
+  team: ["create", "edit", "delete"],
+  member: ["create", "edit", "delete"],
+  invitation: ["create", "delete"],
+  project: ["create", "edit", "delete"],
+  asset: ["view", "create", "edit", "delete"],
+  blogPost: ["create", "edit", "review", "delete"],
+  legal: ["create", "edit", "review", "delete"]
 })
 
 export const member = ac.newRole({
-  ...memberAc.statements
+  ...memberAc.statements,
+  organization: [],
+  team: [],
+  member: [],
+  invitation: [],
+  project: ["create", "edit", "delete"],
+  asset: ["view", "create", "edit", "delete"],
+  blogPost: ["create", "edit", "review", "delete"],
+  legal: ["create", "edit", "review", "delete"]
 })
 
 export const user = ac.newRole({
+  organization: [],
+  team: [],
+  member: [],
+  invitation: [],
+  project: [],
+  asset: [],
   blogPost: [],
   legal: []
 })
