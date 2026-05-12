b542b0333287440ac1ca5e349ae63ade28a68ddf
idantitydotme <88442616+idantitydotme@users.noreply.github.com>
2026-05-12 01:16:51 -0300

    fix: comments out db initialization for debugging

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,5 @@
-import { neon } from "@neondatabase/serverless"
-import { drizzle } from "drizzle-orm/neon-http"
-
-export const db = drizzle({ client: neon(process.env.DATABASE_URL!) })
+// import { neon } from "@neondatabase/serverless"
+// import { drizzle } from "drizzle-orm/neon-http"
+//
+// export const db = drizzle({ client: neon(process.env.DATABASE_URL!) })
+export const db = {} as any
