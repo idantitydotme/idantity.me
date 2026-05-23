fa3f9eeb2c2bc078d4c53104b5f5e48d381e8d93
idantitydotme <88442616+idantitydotme@users.noreply.github.com>
2026-05-23 17:34:51 -0300

    fix: fixes rerouting in construction mode blocker

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,8 @@
 import { neon } from "@neondatabase/serverless"
 import { drizzle } from "drizzle-orm/neon-http"
+import * as schema from "@/db/schema"
 
-export const db = drizzle({ client: neon(process.env.DATABASE_URL!) })
+export * from "@/db/schema"
+export { schema }
+
+export const db = drizzle(neon(process.env.DATABASE_URL!), { schema })
