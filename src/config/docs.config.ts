54fdc40a03817e213daa1ede107b66c957b51276
idantitydotme <88442616+idantitydotme@users.noreply.github.com>
2026-05-26 03:17:07 -0300

    feat: migrates from starlight to in-house implementation

---
+++ src/config/docs.config.ts
@@ -0,0 +1,8 @@
+import { defineDocsConfig } from "@rimelight/ui/utils";
+
+export default defineDocsConfig({
+  editLink: {
+    baseUrl: "https://github.com/idantitydotme/idantity.me/edit/main/src/content/docs/"
+  },
+  sidebar: []
+});
