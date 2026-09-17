b031d951ee2c090d1dd6c86bc448693272548328
idantitydotme <88442616+idantitydotme@users.noreply.github.com>
2026-09-17 17:59:29 -0300

    refactor: moves config files down to rimelight  packages

--- astro.config.ts
+++ astro.config.ts
@@ -4,11 +4,43 @@ import { rimelightAstroConfig } from "@rimelight/config/astro"
 export default defineConfig(
   rimelightAstroConfig({
     domain: "idantity.me",
-    seo: true,
+    seo: {
+      name: "idantity",
+      description: "Welcome to my website!",
+      author: "idantity",
+      branding: {
+        logo: {
+          alt: "idantity"
+        },
+        colors: {
+          themeColor: "#ffffff",
+          backgroundColor: "#ffffff"
+        }
+      },
+      titleTemplate: "%s | idantity",
+      locales: {
+        en: "en-US",
+        pt: "pt-BR"
+      },
+      privatePathPrefixes: [
+        "/dashboard",
+        "/admin",
+        "/cms",
+        "/internal",
+        "/api",
+        "/dev",
+        "/og",
+        "/open-graph",
+        "/auth"
+      ]
+    },
     solid: true,
     cms: true,
     security: true,
-    i18n: true,
+    i18n: {
+      locales: ["en", "es"],
+      defaultLocale: "en"
+    },
     ui: true
   })
 )
