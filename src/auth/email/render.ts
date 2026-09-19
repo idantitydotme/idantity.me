f991b1ee47d4dc02f2d0678184f76b489fe560e6
idantitydotme <88442616+idantitydotme@users.noreply.github.com>
2026-09-19 09:24:32 -0300

    refactor: moves from astro actions to pure hono

--- src/auth/email/render.ts
+++ src/auth/email/render.ts
@@ -1,29 +1,7 @@
 import { experimental_AstroContainer as AstroContainer } from "astro/container"
-import VerificationEmail from "#components/email/VerificationEmail.astro"
-import PasswordResetEmail from "#components/email/PasswordResetEmail.astro"
-import SignupNotificationEmail from "#components/email/SignupNotificationEmail.astro"
 import ContactEmail from "#components/email/ContactEmail.astro"
-import { getEmailMessages } from "./messages"
 
 const container = AstroContainer.create()
-
-export async function renderVerificationEmail(url: string, locale = "en") {
-  return (await container).renderToString(VerificationEmail, {
-    props: { url, messages: getEmailMessages(locale) }
-  })
-}
-
-export async function renderPasswordResetEmail(url: string, locale = "en") {
-  return (await container).renderToString(PasswordResetEmail, {
-    props: { url, messages: getEmailMessages(locale) }
-  })
-}
-
-export async function renderSignupNotificationEmail(signInUrl: string, locale = "en") {
-  return (await container).renderToString(SignupNotificationEmail, {
-    props: { signInUrl, messages: getEmailMessages(locale) }
-  })
-}
 
 export async function renderContactEmail(props: {
   name: string
